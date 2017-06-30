d6708381514bdb900c180348e7e83f3ef25bd210
KostaJevtuc <jeca0102@live.com>
2017-06-30 16:24:43 +0200

    Add files via upload

--- Komentari.fajl.py
+++ Komentari.fajl.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-#print("Don't forget to kys")
+
+promen = "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+promen= promen.lower()
+promen2 = promen.split(" ")
+print(promen2)
+promen3= list(promen)
+print(promen3)
 
 
-""" 
-Ovo je komentar i ovo i ovo
-jej 
-komentari
-volim komentare
-"""
 
-print("Hello world!")
-print("Ovo je druga linija")
