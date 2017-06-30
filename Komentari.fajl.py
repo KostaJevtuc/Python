d0a1a250a1b99d7f6f0cd70b06218072c580b1fe
KostaJevtuc <jeca0102@live.com>
2017-06-30 13:48:46 +0200

    Add files via upload

---
+++ Komentari.fajl.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#print("Don't forget to kys")
+
+
+""" 
+Ovo je komentar i ovo i ovo
+jej 
+komentari
+volim komentare
+"""
+
+print("Hello world!")
+print("Ovo je druga linija")
