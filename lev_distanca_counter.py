4b9325545b2d2c55c890340d275fcb09bb7fe9c2
KostaJevtuc <jeca0102@live.com>
2018-07-20 10:40:21 +0200

    Update lev_distanca_counter.py

--- lev_distanca_counter.py
+++ lev_distanca_counter.py
@@ -1,11 +1,12 @@
 rec1=input('Napisite prvo rec ')
 rec2=input('Napisite pseudo rec ')
 raz_slova=0
-
+rec1=rec1.lower()
+rec2=rec2.lower()
 if len(rec1)!=len(rec2):
-    print('rec i pseudorec moraju biti iste duzine')
+    print('\nRec i pseudorec moraju biti iste duzine')
 else:
     for slovo in range(len(rec1)):
         if not rec1[slovo]==rec2[slovo]:
             raz_slova+=1
-print('levestajnova distanca: ',raz_slova)
+    print('\nlevenstajnova distanca: ',raz_slova)
